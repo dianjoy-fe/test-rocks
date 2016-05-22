913a0956c8a8bc24dd61b7bfff00c442ccedf76c
Meathill <meathill@gmail.com>
2016-05-22 11:34:26 +0800

    整理一下想法

---
+++ js/main.js
@@ -0,0 +1,26 @@
+'use strict';
+
+Reveal.initialize({
+  controls: true,
+  progress: true,
+  history: true,
+  center: true,
+  
+  theme: Reveal.getQueryHash().theme,
+  transition: Reveal.getQueryHash().transition || 'default',
+  
+  dependencies: [
+    {
+      src: 'https://cdnjs.cloudflare.com/ajax/libs/marked/0.3.5/marked.min.js',
+      condition: function () {
+        return !!document.querySelector('[data-markdown]');
+      }
+    },
+    {
+      src: 'https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.3.0/plugin/markdown/marked.min.js',
+      condition: function () {
+        return !!document.querySelector('[data-markdown]');
+      }
+    }
+  ]
+});
